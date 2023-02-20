9c394bc769b43b3bf79645ac8c58a188f24ed1fc
Quiroga10 <juniorquirogamio@gmail.com>
2023-02-20 00:12:29 -0300

    maquetación mobile

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
+//Modo Dark
 const btnSwitch = document.querySelector('#switch');
 
 btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
 });
+
+//Redigir Páginas
+function registrar(){
+    window.location.href = "../registrarse.html";
+}
+function aceptarRegistro() {
+    window.location.href = "home.html";
+}
